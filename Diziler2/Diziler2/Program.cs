b53209f7e38537202eb41d03a0aba6927039dce8
Doğukan <60947113+Dogukan18@users.noreply.github.com>
2021-10-15 03:13:23 +0300

    Add files via upload

---
+++ Diziler2/Diziler2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Diziler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  string [] isimler = { "Ali", "Veli", "Doğukan", "Mehmet", "Ahmet", "Oğuzhan" };
+               Console.WriteLine(isimler[3]);
+               Console.ReadKey(); */
+
+            /*   int[] sayilar = { 50, 60, 70, 80, 90, 40, 20, 10 };
+               Console.WriteLine(sayilar[5]);
+               Console.ReadKey(); */
+
+           /*  string[] sehirler = new string[4];
+              for (int i = 0; i < 4; i++)
+              {
+                  Console.Write("Şehir: ");
+                  sehirler[i] = Console.ReadLine();
+              }
+
+              Console.WriteLine("****Şehirler****");
+
+              for (int i = 0; i < 4 ; i++)
+              {
+                  Console.WriteLine(sehirler[i]);
+              }
+
+              Console.Read(); */
+
+            /*  string[] isim = new string[3];
+              int[] s1 = new int[3];
+              int[] s2 = new int[3];
+              int[] ort = new int[3];
+
+              for (int i = 0; i < 3; i++)
+              {
+                  Console.Write(i + 1 + " Öğrenci Adı: ");
+                  isim[i] = Console.ReadLine();
+
+                  Console.Write(i + 1 + " 1. Sınav:");
+                  s1[i] = Convert.ToInt32(Console.ReadLine());
+
+                  Console.Write(i + 1 + " 2.Sınav: ");
+                  s2[i] = Convert.ToInt32(Console.ReadLine());
+
+                  ort[i] = (s1[i] + s2[i]) / 2;
+
+                  Console.Clear();
+              }
+
+              Console.WriteLine();
+              Console.WriteLine ("Öğrenci   Sınav1  Sınav2   Ortalama");
+
+              for (int i = 0; i < 3; i++)
+              {  
+                  Console.WriteLine("   " + isim[i] + "     " + s1[i] + "     " + s2[i] + "         " + ort[i]);
+              }
+
+              Console.Read(); */
+
+           /* int[] degerler = { 1, 2, 3, 4, 5, 6,7, };
+            int sonuc = 1;
+            foreach (int i in degerler)
+            {
+                sonuc = sonuc * i;
+            }
+            Console.Write(degerler.Length+" Sayısının Faktöriyeli: " + sonuc);
+            Console.ReadLine(); */
+        }
+    }
+}
